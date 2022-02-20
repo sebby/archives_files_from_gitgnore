e7a66a22d493a3ad9227321e101bcbe810062b30
Sebastien Cunin <640614+sebby@users.noreply.github.com>
2022-02-20 17:49:56 +0100

    The character * is recognized, so ignore for the archives.zip

--- create_archive.sh
+++ create_archive.sh
@@ -3,19 +3,24 @@
 filename='.gitignore'
 file_archive='archives.zip'
 rm $file_archive
+b='#'
 result=''
 n=1
 while read line; do
 # reading each line
 if [ -n "$line" ]; then
-    echo "Line No. $n : $line"
-    result+="$line "
+    firstCharacter=${line:0:1}
+    if [ "$firstCharacter" != "$b" ]; then
+      echo "Line No. $n : $line"
+      result+="$line "
+    else
+      echo "This line $n: $line is not used"
+    fi
 fi
 n=$((n+1))
 done < $filename
 
-test='file1.txt'
+echo $result
 if [ -n "$result" ]; then
 zip -r $file_archive $result
-
 fi
