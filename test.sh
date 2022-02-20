ad84e27d856b8c34a96ee078934eb9d4f96beef1
Sebastien Cunin <640614+sebby@users.noreply.github.com>
2022-02-20 17:05:45 +0100

    Create zip file from gitignore files, the * character works too.

--- test.sh
+++ test.sh
@@ -1,12 +1,24 @@
 #!/bin/bash
 # read -p "Enter file name : " filename
-
 filename='.gitignore'
+file_archive='archives.zip'
+rm $file_archive
+result=''
 n=1
 while read line; do
 # reading each line
 if [ -n "$line" ]; then
+  # @todo chercher avec des extensions
+  if [ -n "$line" ]; then
     echo "Line No. $n : $line"
+    result+="$line "
+  fi
 fi
 n=$((n+1))
 done < $filename
+
+test='file1.txt'
+if [ -n "$result" ]; then
+zip -r $file_archive $result
+
+fi
